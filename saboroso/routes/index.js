9eeec15d51f3f16f2382ac56ed980d72196f903b
giovanne <giovanne.aquino.clear@gmail.com>
2023-08-30 13:00:45 -0300

    Termina formatação de include

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -1,48 +1,58 @@
 var conn = require('./../inc/db');
 var express = require('express');
+var menus = require('./../inc/menus')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  conn.query(`
-    SELECT * FROM tb_menus ORDER BY title
-  `,(err, results)=>{
-
-    if(err){
-      console.log(err);
-    }
+  menus.getMenus().then(results=>{
 
     res.render('index', {
       title: 'Restaurante do balacobaco',
       menus: results
     });
   });
-  });
+
+  })
+
   
   router.get('/contacts', function(req, res, next){
     res.render('contacts', {
-      title: 'Contato - Restaurante do balacobaco'
+      title: 'Contato - Restaurante do balacobaco',
+      background: 'images/img_bg_3.jpg',
+      h1: 'Fala truta'
     });
   });
 
   router.get('/menus', function(req, res, next){
+    
+    menus.getMenus().then(results=>{
+    
     res.render('menus',{
-      title: 'Menus - Restaurante do balacobaco'
+      title: 'Menus - Restaurante do balacobaco',
+      background: 'images/img_bg_1.jpg',
+      h1: ' coma imediatamente ',
+      menus: results
 
     });
   });
+});
 
   router.get('/reservations', function(req, res, next){
     res.render('reservations', {
-      title: 'Reservas - Restaurante do balacobaco'
+      title: 'Reservas - Restaurante do balacobaco',
+      background: 'images/img_bg_2.jpg',
+      h1: 'Reserve uma Mesa'
 
     });
   });
 
   router.get('/services', function(req, res, next){
     res.render('services',{
-      title: 'Serviços - Restaurante do balacobaco'
+      title: 'Serviços - Restaurante do balacobaco',
+      background: 'images/img_bg_1.jpg',
+      h1: 'amo trabalhar'
 
     });
   });
