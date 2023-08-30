2e1d768b7fd42f8252efb747754916b701459cdb
giovanne <giovanne.aquino.clear@gmail.com>
2023-08-30 11:21:34 -0300

    Adiciona caminhos ejs para o template do projeto

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -19,6 +19,33 @@ router.get('/', function(req, res, next) {
     });
   });
   });
+  
+  router.get('/contacts', function(req, res, next){
+    res.render('contacts', {
+      title: 'Contato - Restaurante do balacobaco'
+    });
+  });
+
+  router.get('/menus', function(req, res, next){
+    res.render('menus',{
+      title: 'Menus - Restaurante do balacobaco'
+
+    });
+  });
+
+  router.get('/reservations', function(req, res, next){
+    res.render('reservations', {
+      title: 'Reservas - Restaurante do balacobaco'
+
+    });
+  });
+
+  router.get('/services', function(req, res, next){
+    res.render('services',{
+      title: 'Serviços - Restaurante do balacobaco'
+
+    });
+  });
 
 
 module.exports = router;
