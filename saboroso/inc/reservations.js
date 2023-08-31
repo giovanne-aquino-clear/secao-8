81be627320a2d0f56bf028191137ac3385d65922
giovanne <giovanne.aquino.clear@gmail.com>
2023-08-31 11:04:05 -0300

    Adiciona contacts,js e finaliza envio de reserva e contato para o banco de dados

--- saboroso/inc/reservations.js
+++ saboroso/inc/reservations.js
@@ -2,20 +2,50 @@
    
    module.exports = {
 
-        render(req, res, error){
+        render(req, res, error, success){
 
             res.render('reservations', {
                 title: 'Reservas - Restaurante do balacobaco',
                 background: 'images/img_bg_2.jpg',
                 h1: 'Reserve uma Mesa',
                 body: req.body,
-                error
+                error,
+                success
             })
 
         },
 
         save(fields){
 
+            return new Promise((resolve, reject)=>{
+
+                let date = fields.date.split('/');
+                fields.date = `${date[2]}-${date[1]}-${date[0]}`;
+
+                conn.query(`
+                INSERT INTO tb_reservations(name,email,people,date,time)
+                VALUES(?,?,?,?,?)
+            `, [
+                fields.name, 
+                fields.email, 
+                fields.people, 
+                fields.date, 
+                fields.time
+            ], (err, results)=>{
+                
+                if(err){
+                    reject(err);
+                }else{
+                    resolve(results)
+                }
+
+
+            });
+
+
+            });
+
+           
 
         }
 
--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -2,6 +2,8 @@ var conn = require('./../inc/db');
 var express = require('express');
 var menus = require('./../inc/menus')
 var reservations = require('./../inc/reservations')
+var contacts = require('./../inc/contacts')
+
 var router = express.Router();
 
 /* GET home page. */
@@ -20,11 +22,36 @@ router.get('/', function(req, res, next) {
 
   
   router.get('/contacts', function(req, res, next){
-    res.render('contacts', {
-      title: 'Contato - Restaurante do balacobaco',
-      background: 'images/img_bg_3.jpg',
-      h1: 'Fala truta'
-    });
+   
+    contacts.render(req, res);
+
+  });
+
+  router.post('/contacts', function(req, res, next){
+    if(!req.body.name){
+      contacts.render(req, res, "digite o nome");
+    } else if(!req.body.email){
+      contacts.render(req, res, "digite o e-mail");
+    } else if(!req.body.message){
+      contacts.render(req, res, "fala logo fi");
+    }  else{
+
+        contacts.save(req.body).then(results =>{
+  
+          req.body = {}
+  
+          contacts.render(req, res, null, "contato enviado com sucesso!");
+  
+  
+        }).catch(err=>{
+  
+          contacts.render(req, res, err.message);
+  
+        })
+  
+      }
+  
+
   });
 
   router.get('/menus', function(req, res, next){
@@ -60,6 +87,19 @@ router.get('/', function(req, res, next) {
     } else if(!req.body.time){
       reservations.render(req, res, "Selecione a hora");
     } else{
+
+      reservations.save(req.body).then(results =>{
+
+        req.body = {}
+
+        reservations.render(req, res, null, "Reserva realizada com sucesso!");
+
+
+      }).catch(err=>{
+
+        reservations.render(req, res, err);
+
+      })
 
     }
 
