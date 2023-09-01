acf4eb0de8dc5d29db36648da40766f4bcc0592c
giovanne <giovanne.aquino.clear@gmail.com>
2023-09-01 10:48:31 -0300

    Adiciona configuraçoẽs da parte de Adminstração do projeto, configurando as rotas e verificação pelo banco de dados

--- saboroso/app.js
+++ saboroso/app.js
@@ -4,14 +4,28 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var session = require('express-session');
+var RedisStore = require('connect-redis')(session);
+
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var adminRouter = require('./routes/admin');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(session({
+  store: new RedisStore({
+    host: 'localhost',
+    port:6379
+  }),
+  secret: 'clear2022',
+    resave:true,
+    saveUninitialized: true
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +34,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ saboroso/routes/admin.js
@@ -0,0 +1,84 @@
+var express = require("express");
+var users = require("./../inc/users");
+var router = express.Router();
+
+/* GET users listing. */
+
+router.use(function(req, res, next){
+  if(['/login'].indexOf(req.url) === -1 && !req.session.user){
+    res.redirect("/admin/login");
+  }else{
+    next();
+  }
+})
+
+router.get("/", function(req, res, next) {
+
+  res.render("admin/index");
+
+
+});
+
+router.get("/logout",function(req,res,next){
+  delete req.session.user
+
+  res.redirect("/admin/login");
+
+})
+
+router.get("/login", function(req, res, next) {
+
+  users.render(req,res,null);
+
+});
+
+
+router.post('/login',function(req,res,next){
+  if(!req.body.email){
+      users.render(req,res,'Preencha o campo e-mail')
+  }else if(!req.body.password){
+      users.render(req,res,'Preencha o campo senha')
+  }else {
+      users.login(req.body.email, req.body.password).then(user =>{
+          req.session.user = user;
+          res.redirect('/admin')
+      }).catch(err =>{
+          users.render(req,res,err.message || err);
+      })
+  }
+})
+
+router.get("/contacts", function(req, res, next) {
+
+  res.render("admin/contacts");
+
+});
+router.get("/email", function(req, res, next) {
+
+  res.render("admin/email");
+
+});
+
+router.get("/menus", function(req, res, next) {
+
+  res.render("admin/menus");
+
+});
+
+router.get("/reservations", function(req, res, next) {
+
+  res.render("admin/reservations",{
+    date:{}
+  });
+
+});
+
+router.get("/users", function(req, res, next) {
+
+  res.render("admin/users");
+
+});
+  
+  
+
+module.exports = router;
