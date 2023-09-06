0b4a4c8f0394be2103d3ca04b3cf0c14b17e1073
giovanne <giovanne.aquino.clear@gmail.com>
2023-09-06 11:43:46 -0300

    read, edit and delete users

--- saboroso/inc/users.js
+++ saboroso/inc/users.js
@@ -1,48 +1,172 @@
-var conn = require("./db");
 
-
-module.exports ={
+var conn = require('./db')
+module.exports = {
 
     render(req,res,error){
 
-        res.render("admin/login", {
-            body:req.body,
+        res.render('admin/login',{
+
+            body: req.body,
             error
-        });
+
+        })
 
     },
 
-    login(email,password){
-        return new Promise ((resolve,reject)=>{
+    login(email, password){
 
-            conn.query(`
+        return new Promise((resolve,reject) =>{
+
+            conn.query(
+            `
                 SELECT * FROM tb_users WHERE email = ?
-            `,[
+            `
+            ,[
                 email
-               ],(err, results)=>{
-                    if(err){
-                        reject(err)
-                    } else{
+            ],(err,results)=>{
+
+                if(err){
+
+                    reject(err);
+
+                }else{  
+                    
+                    if(!results.length > 0){ 
+                        reject('Usuario ou senha incorretos.');
+                    }else{
+
+                        let row = results[0];
                         
-                        if(!results.length > 0){
-                            reject("Usuario ou senha incorretos.");
+                        if(row.password !== password){
+
+                            reject('Usuario ou senha incorretos.')
+
                         }else{
 
-                            let row = results[0];
-
-                            if(row.password !== password){
-                                reject("Usuário ou senha incorretos.");
-                            }else{
-                                resolve(row);
-                            }
-
+                            resolve(row)
                         }
+
                         
                     }
-                }); 
+
+                }
+
+            })
+
+        })
+
+    },
+
+    getUsers() {
+
+        return new Promise((resolve, reject) => {
+
+            conn.query(
+                `SELECT * FROM tb_users ORDER BY id  `
+                , (err, results) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(results)
+                });
+        })
+
+    },
+
+    delete(id) {
+
+        return new Promise((resolve, reject) => {
+    
+          conn.query('DELETE FROM tb_users WHERE id = ?', [id], (err, results) => {
+    
+            if (err) {
+              reject(err);
+            } else {
+              resolve(results)
+            }
+    
+          })
+      
+        })
+    
+      },
+
+      save(fields) {
+
+        return new Promise((resolve, reject) => {
+
+            console.log('fields:', fields)
+
+            let date;
             
-        });
-    }
+            if(fields.date){
+
+                date = fields.date.split('-')
+                fields.date = `${date[0]}-${date[1]}-${date[2]}`;
+            }
+
+            let query, params
+            if (parseInt(fields.id) > 0) {
+
+                if(fields.password){
+
+                    query = `
+                    UPDATE tb_users
+                    SET
+                    password = ?
+                    WHERE id = ?
+                    `;
+                    params=[fields.password, fields.id]
+    
+
+                }else{
+
+                    query = `
+                    UPDATE tb_users
+                    SET
+                        name = ?,
+                        email = ?
+                    WHERE id = ?
+                    `;
+                    params = [
+
+                        fields.name,
+                        fields.email
+                    ]
+        
+                    params.push(fields.id)
+                    
+                }
+
+
+            } else {
+
+                query =
+
+                    `INSERT INTO tb_users (name  ,email,people,date,time)
+                VALUES(?,?,?,?,?)   
+               `
+                params = [
+                    fields.name,
+                    fields.email,
+                    fields.people,
+                    fields.date,
+                    fields.time
+                
+                ]
+
+            }
+
+
+            conn.query(query, params, (err, results) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(results);
+                }
+            })
+        })
+    },
 
 
 }
--- saboroso/routes/admin.js
+++ saboroso/routes/admin.js
@@ -202,11 +202,51 @@ router.delete("/reservations/:id", function(req, res, next){
     }); 
   
 
-router.get("/users", function(req, res, next) {
+    router.get('/users', function (req, res, next) {
 
-  res.render("admin/users",admin.getParams(req));
+      users.getUsers().then(data =>{
+  
+          res.render('admin/users', admin.getParams(req, {
+              date: {},
+              data,
+              moment
+          }))
+  
+      })
+  
+  })
+  
+  router.delete('/users/:id', function (req, res, next) {
+    
+      users.delete(req.params.id[1]).then(results =>{
+         
+          res.send(results)
+  
+      }).catch (err => {
+  
+          res.send(err)
+  
+      })
+  
+  })
+  
+  
+  router.post('/users', function (req, res, next) {
+     
+      console.log('chega aqqqqq userrrs')
+  
+      users.save(req.fields).then(results => {
+         
+          res.send(results)
+  
+  
+      }).catch(err => {
+         
+         console.log(err)
+  
+      });
 
-});
+    });
   
     
 
